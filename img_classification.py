f61f0185193269af7fc5a0744858b7f419d1888f
becode <dominguezalbermar@gmail.com>
2020-10-27 17:10:31 +0100

    app.py for streamlit deployment

---
+++ img_classification.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import tensorflow.keras
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def classification(img, model):
+    # Load the model
+    model = keras.models.load_model(model)
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    
+    data = np.ndarray(shape=(1, 160, 160, 3), dtype=np.float32)
+    image = img
+    
+    ##esizing the image to be at least 160x160 and then cropping from the center
+    size = (160, 160)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    data[0] = image_array
+    
+    ## Note: The image is not normalized because a normalized layer
+    ## is included in the uploaded model
+
+    
+    # Prediction:
+    
+    prediction = model.predict(data)
+    
+    
+    # Apply a sigmoid since our model returns logits
+    
+    prediction = tf.nn.sigmoid(prediction)
+    prediction = tf.where(prediction < 0.5, 0, 1)
+
+    print('Predictions:\n', prediction)
+    
+    return prediction
+
+#testing
